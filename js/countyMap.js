c2ed877c4580d227be094ec7bc5787e583bae573
Charles-Antoine Perrault <perrault.ca@gmail.com>
2014-09-02 17:33:57 -0700

    clad per city file and separate js sheets

--- js/countyMap.js
+++ js/countyMap.js
@@ -1,28 +1,29 @@
+var selectedCounty;
+
+function countyClass(name) {
+  d3.select(".selected").classed("selected", false);
+  selectedCounty = name;
+  d3.selectAll(".Mcounty")
+    .classed("selected", function(d) {return d.properties.name == name;})
+    .transition().duration(300);
+  d3.selectAll(".Tcounty")
+    .classed("selected", function(d) {return d.County == name;})
+    .transition().duration(300);
+}
+
 d3.json("caFields9.json", function(err, ca) {
 
   var div = d3.select("body").append("div")
-  .attr("class", "tooltip")
+  .attr("class", "mapTooltip")
   .style("opacity", 0);
 
   var radio = d3.select(".ca")
               .append("input")
               .attr("type","Radio")
 
-  var svg = d3.select(".ca");
-
-  var group = svg.selectAll('g')
-          .data(topojson.feature(ca, ca.objects.counties2).features)
-          .enter()
-          .append('g')
-
   var colorTOT = d3.scale.linear()
     .domain([30237, 993455, 12867928])
     .range(['rgb(255,245,96)','rgb(215,211,0)','rgb(35,132,67)'])
-    .interpolate(d3.interpolateRgb);
-
-  var colorACR = d3.scale.linear()
-    .domain([0.01, 0.3228, 0.95])
-    .range(['rgb(255,245,96)','rgb(205,211,0)','rgb(35,132,67)'])
     .interpolate(d3.interpolateRgb);
 
   var colorPOP = d3.scale.linear()
@@ -31,28 +32,38 @@ d3.json("caFields9.json", function(err, ca) {
     .interpolate(d3.interpolateRgb);
 
   var projection = d3.geo.albers()
-      .translate([480, 250])
+      .translate([70, 210])
       .scale(2700)
       .rotate([122.4183, 0])
       .center([0, 37.7750]);
 
+  var svg = d3.select(".ca");
+
+  var group = svg.selectAll('g')
+          .data(topojson.feature(ca, ca.objects.counties2).features)
+          .enter()
+          .append('g')
+
   var path = d3.geo.path().projection(projection);
 
-  var LegendW = 30;
-      LegendH = 80;
+  var LegendW = 80;
+      LegendH = 20;
 
-  var svg = d3.select("body").append("svg:svg")
+  var svg = d3.select("#countyMapSvg").append("svg:svg")
+      .attr("class", "mapLegend")
       .attr("width", LegendW)
       .attr("height", LegendH)
+      .attr("x", 0)
+      .attr("y", 420)
       .style("display", "none");
 
   var gradient = svg.append("svg:defs")
     .append("svg:linearGradient")
       .attr("id", "gradient")
-      .attr("x1", "0%")
+      .attr("x1", "100%")
       .attr("y1", "0%")
       .attr("x2", "0%")
-      .attr("y2", "100%")
+      .attr("y2", "0%")
       .attr("spreadMethod", "pad");
 
   gradient.append("svg:stop")
@@ -72,45 +83,42 @@ d3.json("caFields9.json", function(err, ca) {
 
   svg.append("text")
       .attr("text-anchor", "middle")
-      .attr("x","50%")
-      .attr("y","15%")
+      .attr("x","15%")
+      .attr("y","69%")
       .attr("dy",0)
-      .text("+");
+      .style("font-size", "16px")
+      .style("fill", "black")
+      .text("-");
 
   svg.append("text")
       .attr("text-anchor", "middle")
-      .attr("x","50%")
-      .attr("y","90%")
+      .attr("x","85%")
+      .attr("y","65%")
       .attr("dy",0)
-      .text("-");
+      .style("font-size", "14px")
+      .style("fill", "white")
+      .text("+");
 
-  d3.selectAll(".radio").on("change", function(){
+d3.selectAll(".radio").on("change", function(){
 
-  if (document.getElementById("ac_tot").checked) {
-          counties.transition().duration(250)
-               .style("fill", function (d) {return colorTOT(d.properties.ac_tot);});
-          svg.transition().duration(300).style("display", null);
-               }
+if (document.getElementById("ac_tot").checked) {
+        counties.transition().duration(250)
+             .style("fill", function (d) {return colorTOT(d.properties.ac_tot);});
+        svg.transition().duration(300).style("display", null);
+             }
 
-  else if (document.getElementById("ACR_NORM_1").checked) {
-          counties.transition().duration(250)
-               .style("fill", function (d) {return colorACR(d.properties.ACR_NORM_1);});
-          svg.transition().duration(300).style("display", null);
-               }
+else if (document.getElementById("POP_NORM").checked) {
+        counties.transition().duration(250)
+             .style("fill", function (d) {return colorPOP(d.properties.POP_NORM);});
+        svg.transition().duration(300).style("display", null);
+             }
+           });
 
-  else if (document.getElementById("POP_NORM").checked) {
-          counties.transition().duration(250)
-               .style("fill", function (d) {return colorPOP(d.properties.POP_NORM);});
-          svg.transition().duration(300).style("display", null);
-               }
-             });
-
-  var counties = group.append('path')
-            .attr('d',path)
-            .attr("class", function(d) { return "counties2 " + d.id; })
+  var counties = group.append("path")
+            .attr("d",path)
+            .attr("class", "Mcounty")
             .classed("geo", true)
-            .attr("d", path)
-            .style("fill", "LightGray ")
+            .style("fill", "LightGray")
             .on("mouseover", function(d) {
             div.transition().duration(300).style("opacity", 1);
             div.text(d.properties.name+" County")
@@ -118,11 +126,13 @@ d3.json("caFields9.json", function(err, ca) {
             .style("top", (d3.event.pageY -30) + "px");})
             .on("mouseout", function (d) { div.transition().duration(300).style("opacity", 0);})
             .on("click", function(d) {
-            d3.select(".selected").classed("selected", false);
-            d3.select(this).classed("selected", true).transition().duration(300);
+            selectedCounty = d.properties.name;
+            countyClass(d.properties.name);
             updatePie1(d.properties.name);
             updatePie2(d.properties.name);
             updateCountyName(d.properties.name);
+            updateCountyTot(d3.format(",")(d.properties.ac_tot));
+            updateCountyInh(d3.format(",")(d.properties.POP_NORM));
         });
 
 });
